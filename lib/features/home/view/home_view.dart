d3330094044ea67326a4340f5725a09d5e9ba25e
oguzkaba <oguz-kaba@hotmail.com>
2022-09-20 00:54:16 +0300

    commit-8

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -7,7 +7,6 @@ import '../../product/blocs/categories/categories_bloc.dart';
 
 import '../../product/blocs/products/products_bloc.dart';
 import '../../product/model/products_model.dart';
-import 'package:gradient_borders/gradient_borders.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -36,7 +35,7 @@ class _HomeViewState extends State<HomeView> {
           centerTitle: true,
           title: const Text("LindaWedding - Home"),
           bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(50.0),
+              preferredSize: const Size.fromHeight(44.0),
               child: BlocConsumer<CategoriesBloc, CategoriesState>(
                   listener: (context, state) {
                 if (state is CategoriesError) {
@@ -93,41 +92,31 @@ class _HomeViewState extends State<HomeView> {
                 }
               })),
         ),
-        SliverGrid(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 2,
-          ),
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return Text(index.toString());
-            },
-          ),
-        ),
-        // BlocConsumer<ProductsBloc, ProductsState>(
-        //   listener: (context, state) {
-        //     if (state is ProductsError) {
-        //       final snackBar = SnackBar(
-        //         backgroundColor: Colors.red,
-        //         content: state.error == "XMLHttpRequest error."
-        //             ? const Text("Hata-> (İstekte bulunulan adres hatalı.)")
-        //             : const Text("Hata-> (Bağlantı zaman aşımı..)"),
-        //       );
-        //       ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        //     }
-        //   },
-        //   builder: (context, state) {
-        //     if (state is ProductsInitial) {
-        //       return _buildLoadingWidget();
-        //     } else if (state is ProductsLoading) {
-        //       return _buildLoadingWidget();
-        //     } else if (state is ProductsLoaded) {
-        //       return _buildBodyWidget(context, state.products);
-        //     } else {
-        //       return Container();
-        //     }
-        //   },
-        // )
+        BlocConsumer<ProductsBloc, ProductsState>(
+          listener: (context, state) {
+            if (state is ProductsError) {
+              final snackBar = SnackBar(
+                backgroundColor: Colors.red,
+                content: state.error == "XMLHttpRequest error."
+                    ? const Text("Hata-> (İstekte bulunulan adres hatalı.)")
+                    : const Text("Hata-> (Bağlantı zaman aşımı..)"),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            }
+          },
+          builder: (context, state) {
+            if (state is ProductsInitial) {
+              return _buildSliverLoadingWidget();
+            } else if (state is ProductsLoading) {
+              return _buildSliverLoadingWidget();
+            } else if (state is ProductsLoaded) {
+              return _buildBodyWidget(context, state.products);
+              //return Container();
+            } else {
+              return SliverToBoxAdapter(child: Container());
+            }
+          },
+        )
       ]),
       bottomNavigationBar: BottomNavigationBar(items: const [
         BottomNavigationBarItem(label: "", icon: Icon(Icons.home_rounded)),
@@ -135,69 +124,72 @@ class _HomeViewState extends State<HomeView> {
       ]),
     );
   }
-
-  Center _buildLoadingWidget() =>
-      const Center(child: CircularProgressIndicator());
 }
 
+Widget _buildLoadingWidget() =>
+    const Center(child: CircularProgressIndicator());
+
+Widget _buildSliverLoadingWidget() =>
+    const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()));
+
 Widget _buildBodyWidget(BuildContext context, List<ProductsModel> model) {
-  return SliverGrid.count(
-    crossAxisCount: 3,
-    crossAxisSpacing: 5,
-    mainAxisSpacing: 5,
-    children: model
-        .map((e) => GestureDetector(
-              onTap: () => context.router.push(ProductDetailView(id: e.id)),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.05),
-                          spreadRadius: 5,
-                          blurRadius: 1,
-                          offset: const Offset(
-                              0, 0.2), // changes position of shadow
-                        ),
-                      ],
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Column(
+  return SliverGrid(
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 1, childAspectRatio: 3 / 2),
+    delegate: SliverChildBuilderDelegate(
+      childCount: model.length,
+      (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () =>
+              context.router.push(ProductDetailView(id: model[index].id)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.05),
+                  spreadRadius: 5,
+                  blurRadius: 1,
+                  offset: const Offset(0, 0.2), // changes position of shadow
+                ),
+              ], color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                children: [
+                  Image.network(model[index].image,
+                      fit: BoxFit.contain, height: 180),
+                  Text(model[index].title, overflow: TextOverflow.ellipsis),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Image.network(e.image, fit: BoxFit.contain, height: 180),
-                      Text(e.title, overflow: TextOverflow.ellipsis),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          for (var i = 0; i < 5; i++)
-                            Icon(
-                              e.rating.rate.round() > i
-                                  ? Icons.star
-                                  : Icons.star_border_outlined,
-                              color: Colors.amber,
-                              size: 14,
-                            ),
-                          Text(" ( ${e.rating.count} )",
-                              overflow: TextOverflow.ellipsis),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "${e.price} TL",
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16),
+                      for (var i = 0; i < 5; i++)
+                        Icon(
+                          model[index].rating.rate.round() > i
+                              ? Icons.star
+                              : Icons.star_border_outlined,
+                          color: Colors.amber,
+                          size: 14,
                         ),
-                      ),
+                      Text(" ( ${model[index].rating.count} )",
+                          overflow: TextOverflow.ellipsis),
                     ],
                   ),
-                ),
+                  const SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "${model[index].price} TL",
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                  ),
+                ],
               ),
-            ))
-        .toList(),
+            ),
+          ),
+        );
+      },
+    ),
   );
 }
